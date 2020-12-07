5c25688a4fb5ffdf8a4a1da08836709d83237f6a
varleg <johninge@varleg.no>
2020-12-07 10:03:11 +0100

    rebuild fynction

--- HttpTriggerPrint/index.js
+++ HttpTriggerPrint/index.js
@@ -1,4 +1,13 @@
-const df = require("durable-functions");
 module.exports = async function (context, req) {
-    context.log(process.env.LETS_TEST);
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
 }
